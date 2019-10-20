2e98ba1b75d7a15742d6f75933fafba7e9004064
ariel <arielalmeidalopes@gmail.com>
2019-10-20 15:10:32 -0300

    Alteração na listagem de pessoas para mostrar o objetivo dela para facilitar no momento do treino

--- resources/views/admin/listarPessoas.blade.php
+++ resources/views/admin/listarPessoas.blade.php
@@ -15,15 +15,17 @@
         <table class="table table-bordered table-hover table-striped">
             <tr style="width: 100%">
                 <th style="width: 5%">ID</th>
-                <th style="width: 35%">Nome</th>
-                <th style="width: 35%">Tipo</th>
-                <th>Ação</th>
+                <th style="width: 28,33%">Nome</th>
+                <th style="width: 28,33%">Objetivo</th>
+                <th style="width: 28,33%">Tipo</th>
+                <th style="width: 10%">Ação</th>
             </tr>
             @if($pessoas != NULL)
                 @foreach($pessoas as $pessoa)
                 <tr>
                     <td>{{$pessoa->id}}</td>
                     <td>{{$pessoa->nome}}</td>
+                    <td>{{$pessoa->objetivo->nome}}</td>
                     <td>{{$pessoa->tipo}}</td>
                     <td>
                         <a href="/admin/pessoa/{{$pessoa->id}}/editar" class="btn btn-primary"><i class="fas fa-edit"></i></a>
